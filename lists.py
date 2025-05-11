e3162e9f366ff69011e261e0526a22d06ec4e34a
GuadalupeFernandezG <gfernandezgarcia@mail.austral.edu.ar>
2025-05-10 23:26:52 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -36,4 +36,29 @@ def check_lists (lista1,lista2):
 		return False
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+	list1= list_of_lists_to_modify [0]
+	list2= list_of_lists_to_modify [1]
+	list3= list_of_lists_to_modify [2]
+
+	if len(list_of_lists_to_modify)==3:
+		lista1= list_of_lists_to_modify [0]
+		lista2= list_of_lists_to_modify [1]
+		lista3= list_of_lists_to_modify [2]
+
+	elif len(list_of_lists_to_modify)!=3:
+		return "La lista no tiene tres listas"
+
+	if len(list1)>=2:
+		lista1=lista1[0:2]
+
+	elif len(lista1)<2:
+		lista1= lista1[0:1]
+		return lista1
+
+	if len(lista2)>=4:
+		lista2=lista2[1:4]
+	elif len(lista2)<4:
+		lista2=lista2[1:]
+		return lista2
+	lista3=lista3[-2:]
+	return lista1, lista2, lista3
