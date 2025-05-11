286ede7c22a17d8cae7cda71ff79e7f68009675d
GuadalupeFernandezG <gfernandezgarcia@mail.austral.edu.ar>
2025-05-10 23:32:32 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -35,30 +35,22 @@ def check_lists (lista1,lista2):
 	else: 
 		return False
 
-def list_of_lists(list_of_lists_to_modify):
-	list1= list_of_lists_to_modify [0]
-	list2= list_of_lists_to_modify [1]
-	list3= list_of_lists_to_modify [2]
-
-	if len(list_of_lists_to_modify)==3:
-		lista1= list_of_lists_to_modify [0]
-		lista2= list_of_lists_to_modify [1]
-		lista3= list_of_lists_to_modify [2]
-
-	elif len(list_of_lists_to_modify)!=3:
-		return "La lista no tiene tres listas"
-
-	if len(list1)>=2:
-		lista1=lista1[0:2]
-
-	elif len(lista1)<2:
-		lista1= lista1[0:1]
-		return lista1
-
-	if len(lista2)>=4:
-		lista2=lista2[1:4]
-	elif len(lista2)<4:
-		lista2=lista2[1:]
-		return lista2
-	lista3=lista3[-2:]
-	return [lista1, lista2, lista3]
+def list_of_lists(list_of_lists):
+    if len(list_of_lists) >= 3:
+        lista1 = list_of_lists[0]
+        lista2 = list_of_lists[1]
+        lista3 = list_of_lists[2]
+    elif len(list_of_lists) < 3:
+        return "la lista no tiene tres listas"
+    if len(lista1) >= 2:
+        lista1 = lista1[0:2]
+    elif len(lista1) < 2:
+        lista1 = lista1[0:1]
+        return lista1
+    if len(lista2) >= 4:
+        lista2 = lista2[1:4]
+    elif len(lista2) < 4:
+        lista2 = lista2[1:]
+        return lista2
+    lista3 = lista3[-2:]
+    return lista1, lista2, lista3
